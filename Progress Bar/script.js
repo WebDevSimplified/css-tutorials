dca07cb6c51869ccb2064a0a7e7e92ac8b0c6cc2
Web Dev Simplified <webdevsimplified@gmail.com>
2019-02-24 19:16:38 -0600

    Initial Progress Bar

---
+++ Progress Bar/script.js
@@ -0,0 +1,6 @@
+const progressBar = document.getElementsByClassName('progress-bar')[0]
+setInterval(() => {
+  const computedStyle = getComputedStyle(progressBar)
+  const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+  progressBar.style.setProperty('--width', width + .1)
+}, 5)
